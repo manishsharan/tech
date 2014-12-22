bffa2a21eb5f59e7947d691e9187b04c07018cf6
Manish Sharan <manish.sharan@flipkarts-MacBook-Pro.local>
2014-12-22 16:35:09 +0530

    SherlockBeast

--- src/com/hackerrank/random/SherlockBeast.java
+++ src/com/hackerrank/random/SherlockBeast.java
@@ -8,69 +8,56 @@ import java.util.Scanner;
  */
 public class SherlockBeast {
 
-    public static final int cutoff = 5;
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String numTestCase = scanner.nextLine();
         for(int i=0;i<Integer.valueOf(numTestCase);i++){
             String value = scanner.nextLine();
-            System.out.println(getDecentNum(Integer.valueOf(value)));
+            int valueNum = Integer.valueOf(value);
+            if(valueNum>2) {
+                int numFives = getDecentNum(valueNum);
+                if(numFives>=0){
+                    for (int j = 0; j < numFives; j=j+3) {
+                        System.out.print("555");
+                    }
+                    for (int k = 0; k < valueNum-numFives; k=k+5) {
+                        System.out.print("33333");
+                    }
+                }
+                else System.out.print("-1");
+            }else System.out.print("-1");
+            System.out.println();
         }
     }
 
-    public static BigInteger getDecentNum(Integer value){
-        if(value<3){
-            return new BigInteger("-1");
-        }if(value==3){
-            return new BigInteger("555");
+    public static int getDecentNum(Integer value){
+        if(value==3){
+            return 3;
         }if(value>=5) {
             if (value % 5 == 0 && value % 3 == 0){
-                StringBuilder stringBuilder = new StringBuilder();
-                for(int i=0;i<value/5;i++){
-                    stringBuilder.append("55555");
-                }
-                return new BigInteger(stringBuilder.toString());
+                return value;
             }
-            BigInteger returned = new BigInteger("0");
             if (value % 3 != 0) return calculateOnFrequentReductions(value);
             if (value % 3 == 0) {
-                StringBuilder stringBuilder = new StringBuilder();
-                int iterations = value / 3;
-                for (int i = 0; i < iterations; i++) {
-                    stringBuilder.append("555");
-                }
-                return new BigInteger(stringBuilder.toString());
+                return value;
             }
             if (value % 5 == 0) {
-                StringBuilder stringBuilder = new StringBuilder();
-                int iterations = value / 5;
-                for (int i = 0; i < iterations; i++) {
-                    stringBuilder.append("33333");
-                }
-                return new BigInteger(stringBuilder.toString());
+                return 0;
             }
         }
-        return new BigInteger("-1");
+        return -1;
     }
 
-    public static BigInteger calculateOnFrequentReductions(int value){
-        int reduced = 0;
-        StringBuilder stringBuilder = new StringBuilder("0");
+    public static int calculateOnFrequentReductions(int value){
+        int reduced = 5;
         while((value-reduced)%3!=0){
             reduced += 5;
         }
         if(reduced>value){
-            return new BigInteger("-1");
+            return -1;
         }
         else{
-            for(int i=0;i<value-reduced;i++){
-                stringBuilder.append("5");
-            }
-            for(int i=0;i<reduced;i=i+5){
-                stringBuilder.append("33333");
-            }
-            return new BigInteger(stringBuilder.toString());
+            return value-reduced;
         }
     }
 }
